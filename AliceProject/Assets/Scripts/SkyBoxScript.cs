f1fdeec226972e0ceaee266462be1f1c30edd8ab
Jiwoo Lee <ooketmon@likelion.org>
2023-05-23 01:30:02 +0900

    scene에 따라 background 바뀌도록
    ㅈㄱㄴ

---
+++ AliceProject/Assets/Scripts/SkyBoxScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class SkyBoxScript : MonoBehaviour
+{
+
+    public Material skyEntrance;
+    public Material skyExit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeBackground();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void ChangeBackground()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        switch (scene.name)
+        {
+            case "Entrance":
+                RenderSettings.skybox = skyEntrance;
+                break;
+            case "Exit":
+                RenderSettings.skybox = skyExit;
+                break;
+                    
+        }
+
+    }
+}
